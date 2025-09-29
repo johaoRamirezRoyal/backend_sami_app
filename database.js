dea98103398d75157665cd83fcc9d3dfca591acd
johaoRamirezRoyal <hernando.ramirez@royalschool.edu.co>
2025-09-29 10:24:27 -0500

    Samiapp database

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const data = {
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT, 
+    host: process.env.DB_HOST || "localhost", 
+    user: process.env.DB_USER || "samiuser",
+    password: process.env.DB_PASSWORD || "Angel1413@.-+#",
+    database: process.env.DB_DATABASE || "sami_royal",
+    port: process.env.DB_PORT || "3306", 
 }
 
 const pool = mysql.createPool({
